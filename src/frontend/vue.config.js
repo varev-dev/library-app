d98482f0e6776fcf3aae51d8c8cc24bd11d6e15b
Kacper Doga <doga.kacper@gmail.com>
2022-08-13 17:29:12 +0200

    changed backend port

--- src/frontend/vue.config.js
+++ src/frontend/vue.config.js
@@ -7,7 +7,7 @@ module.exports = defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'http://localhost:8081',
         ws: true,
         changeOrigin: true
       }
